9765afbe0b2682c599238f7d2793cd1f874a6d3f
bessax <andrebessax@gmail.com>
2023-11-06 23:35:32 -0300

    #Ajustes: -Realizada a adição de imagens ; - No cardArtista Ajustar e colocar o MudCard, criar o método de exclusão e adição de artistas;

--- ScreenSound.BlazorApp/Program.cs
+++ ScreenSound.BlazorApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScreenSound.BlazorApp;
 using ScreenSound.BlazorApp.Servicos;
 
@@ -8,6 +9,8 @@ using ScreenSound.BlazorApp.Servicos;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
